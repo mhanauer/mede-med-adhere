7fe2eea623b430f9aaabda366f3e2aab13130066
mhanauer <matthewhanauer99@gmail.com>
2024-08-08 03:38:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -8,7 +7,6 @@ data = {
     'member_id': range(1, 101),
     'age': np.random.randint(18, 85, size=100),
     'gender': np.random.choice(['Male', 'Female'], 100),
-    'allowed_pmpm': np.random.uniform(100, 5000, size=100)
 }
 
 df = pd.DataFrame(data)
@@ -21,12 +19,6 @@ chronic_conditions = [
 
 # Assign real chronic condition names to the dataframe
 df['chronic_condition'] = np.random.choice(chronic_conditions, 100)
-
-# Calculate the 75th percentile for allowed_pmpm
-high_cost_threshold = np.percentile(df['allowed_pmpm'], 75)
-
-# Add a column for current high cost claimant based on the 75th percentile
-df['current_high_cost_claimant'] = df['allowed_pmpm'] > high_cost_threshold
 
 # Add a column for current medication adherence
 np.random.seed(42)  # For reproducibility
@@ -62,7 +54,6 @@ df['predicted_med_adherence'] = df['predicted_med_adherence'].round(2)
 st.title('Medication Adherence Risk Prediction')
 
 st.markdown("""
-
 Current non-medication adherence: Members who are in the bottom 25th percentile for current medication adherence. These members are considered non-adherent based on current data.
 
 Below is the medication adherence prediction demo. We predict the probability of a member adhering to their medication (e.g., picking up their medication). We created four categories to help users identify which members are impactable:
@@ -71,10 +62,9 @@ Impactable low medication adherence: Currently not adhering to medication; howev
 
 Unavoidable low medication adherence: Members who are non-adherent and predicted to stay non-adherent. These are members users may want to consider ignoring since there is little opportunity for improvement.
 
-Future low medication adherence: Members who are currently adhering to medication, however, are predicted to drop. Users may want to target these members as they could decrease medication adherence in the future.
+Future low medication adherence: Members who are currently adhering to medication; however, are predicted to drop. Users may want to target these members as they could decrease medication adherence in the future.
 
 Stable high medication adherence: Members with current high medication adherence and predicted to stay high. No intervention with these members is likely necessary.
-
 """)
 
 # Select chronic condition
